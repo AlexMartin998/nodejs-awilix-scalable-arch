44f2a1fddb3cb2dfdd8ab74ea91999a1d87f599a
Adrian Changalombo <alextravc.342@gmail.com>
2023-07-14 23:00:47 -0500

    [ADD]: idea controller

---
+++ src/app/controllers/idea.controller.js
@@ -0,0 +1,56 @@
+let _ideaService;
+
+export class IdeaController {
+  constructor({ IdeaService }) {
+    _ideaService = IdeaService;
+  }
+
+  async findOne(req, res) {
+    const { id } = req.params;
+    const idea = await _ideaService.findOne(id);
+
+    return res.status(200).json(idea);
+  }
+
+  async findAll(_req, res) {
+    const ideas = await _ideaService.getAll();
+
+    return res.status(200).json(ideas);
+  }
+
+  async update(req, res) {
+    const { body } = req;
+    const { id } = req.params;
+    const updatedIdea = await _ideaService.update(id, body);
+
+    return res.status(200).json(updatedIdea);
+  }
+
+  async remove(req, res) {
+    const { id } = req.params;
+    await _ideaService.remove(id);
+
+    return res.status(200);
+  }
+
+  async findAllByAuthor(req, res) {
+    const { userId } = req.params;
+    const authorIdeas = await _ideaService.findAllByAuthor(userId);
+
+    return res.status(200).json(authorIdeas);
+  }
+
+  async upVoteIdea(req, res) {
+    const { id } = req.params;
+    const idea = await _ideaService.upVoteIdea(id);
+
+    return res.status(200).json(idea);
+  }
+
+  async downVoteIdea(req, res) {
+    const { id } = req.params;
+    const idea = await _ideaService.downVoteIdea(id);
+
+    return res.status(200).json(idea);
+  }
+}
--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,1 +1,3 @@
 export * from './home.controller.js';
+export * from './idea.controller.js';
+export * from './user.controller.js';
