ebeb719744b2a7abbe73157b4c20222971521eb6
Adrian Changalombo <alextravc.342@gmail.com>
2023-07-14 22:23:55 -0500

    [ADD]: register services in dependency injection container

--- src/config/container.js
+++ src/config/container.js
@@ -3,7 +3,12 @@ import { asClass, asFunction, asValue, createContainer } from 'awilix';
 import { HomeController } from '../app/controllers/index.js';
 import { HomeRoutes, routerApp } from '../app/routes/index.js';
 import { Server } from '../server.js';
-import { HomeService } from './../app/services/index.js';
+import {
+  CommentService,
+  HomeService,
+  IdeaService,
+  UserService,
+} from './../app/services/index.js';
 import { Comment, Idea, User } from './../data/models/index.js';
 import {
   CommentRepository,
@@ -22,6 +27,9 @@ container
   })
   .register({
     HomeService: asClass(HomeService).singleton(),
+    CommentService: asClass(CommentService).singleton(),
+    IdeaService: asClass(IdeaService).singleton(),
+    UserService: asClass(UserService).singleton(),
   })
   .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton(),
