6abd0e2c8136cfda848b3968699617b494e2fb41
Adrian Changalombo <alextravc.342@gmail.com>
2023-07-15 11:40:25 -0500

    [ADD]: index routes configured

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import 'express-async-errors';
 
-export default function ({ HomeRoutes, UserRoutes }) {
+export default function ({
+  HomeRoutes,
+  UserRoutes,
+  IdeaRoutes,
+  CommentRoutes,
+}) {
   const router = express.Router();
   const apiRoutes = express.Router();
 
   apiRoutes.use('/home', HomeRoutes);
   apiRoutes.use('/users', UserRoutes);
+  apiRoutes.use('/ideas', IdeaRoutes);
+  apiRoutes.use('/comments', CommentRoutes);
 
   router.use('/v1/api', apiRoutes);
   return router;
